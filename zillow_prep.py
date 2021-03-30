43d76fd9453dd611144f18f56c6b923704f430af
Barb Marques <barbara.ann.marques@gmail.com>
2021-03-29 20:26:48 -0500

    Ancilliary function to zillow clustering -- still needs revisions

---
+++ zillow_prep.py
@@ -0,0 +1,30 @@
+def nulls_by_col(df):
+    '''
+    This function takes in a dataframe of observations and attributes and returns a dataframe where each row is an attribute name, 
+    the first column is the number of rows with missing values for that attribute, and the second column is percent of total rows 
+    that have missing values for that attribute. 
+    '''
+    
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    pct_missing = num_missing / rows
+    cols_missing = pd.DataFrame({'number_missing_rows': num_missing, 'percent_rows_missing': pct_missing})
+    return cols_missing
+
+
+
+def cols_missing(df):
+    '''
+    This function takes in a dataframe and returns a dataframe with 3 columns: the number of columns missing, 
+    percent of columns missing, and number of rows with n columns missing. 
+    '''
+    
+    df2 = pd.DataFrame(df.isnull().sum(axis =1), columns = ['num_cols_missing']).reset_index()\
+    .groupby('num_cols_missing').count().reset_index().\
+    rename(columns = {'index': 'num_rows' })
+    df2['pct_cols_missing'] = df2.num_cols_missing/df.shape[1]
+    return df2
+
+
+
+def prep_zillow(df_)
