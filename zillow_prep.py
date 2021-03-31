131c0601a945cb12ec1d4fd404de06f85376e1e4
Barb Marques <barbara.ann.marques@gmail.com>
2021-03-30 22:56:57 -0500

    Revised following walkthrough

--- zillow_prep.py
+++ zillow_prep.py
@@ -27,4 +27,10 @@ def cols_missing(df):
 
 
 
-def prep_zillow(df_)
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
